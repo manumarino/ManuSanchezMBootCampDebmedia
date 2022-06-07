1a429194d1823d4d1851320d272daebc9b99d89e
manumarino <31449057+manumarino@users.noreply.github.com>
2022-06-07 03:10:31 -0300

    segunda modificacion

--- src/Berzerker.java
+++ src/Berzerker.java
@@ -17,18 +17,8 @@ public class Berzerker extends Guerrero{
             System.out.println("desarmado");
         }
     }
-
-    public boolean cargaArma(Arma arma) {
-        if (arma.getClass() == Espada.class) {
-            this.arma = arma;
-            System.out.println("LE METI EL ARMA" + arma.nombre);
-            return true;
-        }
-        else {
-
-            System.out.println("Soy Berzerker y solo utilizo Espada");
-            return false;
-        }
+    int numeroCualquiera = 0;
+ //BORRE cargarArma()
 
     }
 }
